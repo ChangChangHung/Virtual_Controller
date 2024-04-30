7a120de17fce56775b4b26cfec3f6a41a433258b
ChangChangHung <157861616+ChangChangHung@users.noreply.github.com>
2024-04-30 11:36:56 +0800

    Update gpio_controller.py

--- gpio_controller.py
+++ gpio_controller.py
@@ -1,78 +1,65 @@
-try:
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BCM)
-    # 其他與GPIO相關的初始化
-except ImportError:
-    print("RPi.GPIO 模塊未安裝，無法使用 GPIO 功能。")
-
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+# 其他與GPIO相關的初始化
 import time
 
 class GPIOController:
     def __init__(self):
-        # 設定 GPIO pin
-        self.servo_pwm = 18  # 伺服馬達的 GPIO pin
-        self.motor_pin = 17  # 變速馬達的 GPIO pin
-        self.motor_pin = 16  # 變速馬達的 GPIO pin
+        self.in1=22#I/O
+        self.in2=23
+        self.in3=24
+        self.in4=25
+        self.ena1=12#PWM
+        self.enb1=13
+        self.ena2=26
+        self.enb2=27
+        self.servo=5
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.in1,GPIO.OUT)
+        GPIO.setup(self.in2,GPIO.OUT)
+        GPIO.setup(self.in3,GPIO.OUT)
+        GPIO.setup(self.in4,GPIO.OUT)
+        GPIO.setup(self.ena1,GPIO.OUT)
+        GPIO.setup(self.enb1,GPIO.OUT)
+        GPIO.setup(self.ena2,GPIO.OUT)
+        GPIO.setup(self.enb2,GPIO.OUT)
+        GPIO.setup(self.servo,GPIO.OUT)
 
-        # 初始化 GPIO
-        try:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(self.servo_pin, GPIO.OUT)
-            GPIO.setup(self.motor_pin, GPIO.OUT)
-        except NameError:
-            print("'GPIO' is not defined")
+        GPIO.output(self.in1,GPIO.LOW)
+        GPIO.output(self.in2,GPIO.LOW)
+        GPIO.output(self.in3,GPIO.LOW)
+        GPIO.output(self.in4,GPIO.LOW)
 
-        # 創建 PWM 對象
-        try:
-            self.servo_pwm = GPIO.PWM(self.servo_pin, 50)  # 50 Hz 適用於伺服馬達
-            self.motor_pwm = GPIO.PWM(self.motor_pin, 1000)  # 1000 Hz 適用於變速馬達
-        except NameError:
-            print("'GPIO' is not defined")
-
-        # 啟動 PWM
+        self.servo_pwm = GPIO.PWM(self.servo, 50)
+        self.motor_pwm1 = GPIO.PWM(self.ena1, 1000)
+        self.motor_pwm2 = GPIO.PWM(self.enb1, 1000)
+        self.motor_pwm3 = GPIO.PWM(self.ena2, 1000)
+        self.motor_pwm4 = GPIO.PWM(self.enb2, 1000)
         self.servo_pwm.start(0)
-        self.motor_pwm.start(0)
+        self.motor_pwm1.start(0)
+        self.motor_pwm2.start(0)
+        self.motor_pwm3.start(0)
+        self.motor_pwm4.start(0)
+    def Forward(self):
+        GPIO.output(self.in1,GPIO.HIGH)
+        GPIO.output(self.in2,GPIO.HIGH)
+        GPIO.output(self.in3,GPIO.HIGH)
+        GPIO.output(self.in4,GPIO.HIGH)
 
-
-    def turn_left(self):
-        self.servo_pwm.ChangeDutyCycle(5)
-
-    def turn_right(self):
-        self.servo_pwm.ChangeDutyCycle(10) 
-
-    def speed_up(self):
-        self.motor_pwm.ChangeDutyCycle(50)
-
-    def speed_down(self):
-        # 控制變速馬達減速
-        self.motor_pwm.ChangeDutyCycle(25)  # 調整這個值以達到合適的速度
-
+    def turn_change(self,x):
+        self.servo_pwm.ChangeDutyCycle(x)#1-50
+    def speed_change(self,x):
+        self.motor_pwm1.ChangeDutyCycle(x)#1-1000
+        self.motor_pwm2.ChangeDutyCycle(x)
+        self.motor_pwm3.ChangeDutyCycle(x)
+        self.motor_pwm4.ChangeDutyCycle(x)
     def stop_motors(self):
-        # 停止 PWM
         self.servo_pwm.stop()
-        self.motor_pwm.stop()
-
-        # 釋放 GPIO 資源
+        self.motor_pwm1.stop()
+        self.motor_pwm2.stop()
+        self.motor_pwm3.stop()
+        self.motor_pwm4.stop()
         GPIO.cleanup()
 
-    def record_action(self, action):
-        # 將動作寫入文件
-        with open('actions.txt', 'a') as file:
-            file.write(f'{action}\n')
-
-# 使用例子
 if __name__ == "__main__":
     gpio_controller = GPIOController()
-
-    try:
-        gpio_controller.turn_left()
-        time.sleep(2)
-        gpio_controller.turn_right()
-        time.sleep(2)
-        gpio_controller.speed_up()
-        time.sleep(2)
-        gpio_controller.speed_down()
-        time.sleep(2)
-
-    except KeyboardInterrupt:
-        gpio_controller.stop_motors()
